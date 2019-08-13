6e69e0ee3890fc541cb0a73602e763451bb2d7bc
Timothy Rines <tsrines@gmail.com>
2019-08-13 18:24:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-function currentLine(line) {
-  if (!line.length) {
-    return "The line is currently empty."
-  }
 
-  const numbersAndNames = []
-
-  for (let i = 0, l = line.length; i < l; i++) {
-    numbersAndNames.push(`${i + 1}. ${line[i]}`)
-  }
-
-  return `The line is currently: ${numbersAndNames.join(', ')}`
-}
 
 function nowServing(line) {
   if (!line.length) {
@@ -24,4 +12,37 @@ function takeANumber(line, name) {
   line.push(name)
 
   return `Welcome, ${name}. You are number ${line.length} in line.`
+  
 }
+
+var counter = 0
+
+function takeANumbertwo(counter){
+  
+  let i = 0, l = line.length; i < l; i++) 
+    
+  
+  return "Welcome your ticket number, ${name}"
+}
+
+
+
+
+
+function currentLine(line) {
+  if (!line.length) {
+    return "The line is currently empty."
+  }
+
+  const numName = []
+
+  for (let i = 0, l = line.length; i < l; i++) {
+    numName.push(`${i + 1}. ${line[i]}`)
+  }
+
+  console.log(`The line is currently: ${numName.join(', ')}`)
+}
+
+
+currentLine(["Steve", "Frank", "Joe"])
+  
